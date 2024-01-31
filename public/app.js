988dbac6398ba94812f3291a2f000ad520e57cd3
mio Manns <miomanns265@gmail.com>
2024-01-31 23:47:18 +0100

    working text coloring

--- public/app.js
+++ public/app.js
@@ -1,8 +1,28 @@
 
 
-var mainText = document.getElementsByClassName("main-text")[0];
-var input = document.getElementById("input");
+const input = document.getElementsByClassName("input")[0];
+const textDisplay = document.getElementsByClassName("text-display")[0];
+const textParagraph = textDisplay.getElementsByTagName("p")[0];
+const initialText = textParagraph.textContent;
 
-input.onclick = (event) => {
-    console.log("button clicked!");
-} 
+var inputtedChars = ""; // NOT used right now
+input.oninput = (event) => {
+    console.log(event); 
+    if (event.data != null) {
+        inputtedChars += event.data;
+
+        let character = textParagraph.textContent[0];
+        let colorClass = "green";
+        if (event.data != character) {
+            colorClass = "red";
+        }
+
+        let charSpan = document.createElement("span");
+        charSpan.textContent = character;
+        charSpan.classList.add(colorClass);
+        charSpan.classList.add("universal-font");
+
+        textParagraph.textContent = textParagraph.textContent.slice(1); // remove first char 
+        textDisplay.insertBefore(charSpan, textParagraph);
+    }
+}
