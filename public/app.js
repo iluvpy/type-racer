98dbeed22b6f40c398998cefd58098977b9ad5de
mio Manns <miomanns265@gmail.com>
2024-01-31 16:16:43 +0100

    basic input

---
+++ public/app.js
@@ -0,0 +1,8 @@
+
+
+var mainText = document.getElementsByClassName("main-text")[0];
+var input = document.getElementById("input");
+
+input.onclick = (event) => {
+    console.log("button clicked!");
+} 
