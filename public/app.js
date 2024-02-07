90628890ec8b0ca974f0f8140142eb6e3ee7ae17
mio Manns <miomanns265@gmail.com>
2024-02-07 17:12:28 +0100

    small refactor + bug fix

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ const textArea = document.getElementsByClassName("input")[0];
 const textDisplay = document.getElementsByClassName("text-display")[0];
 const textParagraph = textDisplay.getElementsByTagName("p")[0];
 const initialText = textParagraph.textContent;
+const textCursor = document.getElementsByClassName("text-cursor")[0];
 
 function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
@@ -12,14 +13,14 @@ function setCharAt(str,index,chr) {
 
 var inputtedChars = ""; // NOT used right now
 textArea.oninput = (event) => {
-    console.log(event); 
     if (event.data != null) {
         
         let currentChar = textParagraph.textContent[0];
         let colorClass = "green";
-        if (event.data != currentChar) {
+        if (event.data != currentChar) { // typo/misspell 
             colorClass = "red";
         }
+
 
         let charSpan = document.createElement("span");
         charSpan.textContent = currentChar;
@@ -29,14 +30,11 @@ textArea.oninput = (event) => {
         textParagraph.textContent = textParagraph.textContent.slice(1); // remove first char 
         textDisplay.insertBefore(charSpan, textParagraph);
 
-        // update input value to avoid two characters on top of eachother
         textArea.value = setCharAt(textArea.value, textArea.value.length - 1, currentChar);
     }
-    else if (event.inputType == "deleteContentBackward") {
-        console.log("deleting!!!!");
-        let spans = textDisplay.getElementsByTagName("span");
-        console.log(spans);
-        if (spans.length) {
+    else if (event.inputType == "deleteContentBackward") { // delete last last
+        let spans = textDisplay.getElementsByTagName("span"); 
+        if (spans.length > 0) {
             let lastSpan = spans[spans.length - 1];
             let char = lastSpan.textContent;
             textParagraph.textContent = char + textParagraph.textContent; 
@@ -45,7 +43,8 @@ textArea.oninput = (event) => {
 
     }
 
-    // move text cursor the end of the text in input
+    // move text cursor the end of the text input
     setTimeout(() => { textArea.selectionStart = textArea.selectionEnd = 10000; }, 0);
     event.preventDefault();
+
 }
