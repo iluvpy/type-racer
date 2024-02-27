e4455763f299f8c6b5032b0ee26db0e2926c13ba
mio Manns <miomanns265@gmail.com>
2024-02-27 20:05:38 +0100

    a little buggy cursor movment

--- public/app.js
+++ public/app.js
@@ -1,35 +1,84 @@
 
-const CURSOR_AT_START = 0; // text cursor is at the first letter of a word
-const CURSOR_IN_CENTER = 1; // text cursor is in the center of a word
-const CURSOR_AT_END = 1; // text cursor is at the end of a word
 
 const words = document.getElementsByClassName("word");
-words[0].name = CURSOR_AT_START;
 
-var cursorPos = 0; // index of cursor
+var cursorWordPos = 0; // index of cursor
+var cursorLetterPos = 0;
+
+function removeCharAtIndex(index, str) {
+    return str.substring(0, index) + str.substring(index + 1, str.length);
+}
+
+function splitString(index, str) {
+    return [str.substring(0, index), str.substring(index + 1, str.length)];
+}
+
+function removeCursor() {
+    let spanWithCursor = words[cursorWordPos];
+    spanWithCursor.innerHTML = spanWithCursor.textContent;
+}
+
+function updateCursor() {
+    removeCursor();
+    let spanWithCursor = words[cursorWordPos];
+    let letterWithCursor = spanWithCursor.textContent[cursorLetterPos];
+    let splitText = splitString(cursorLetterPos, spanWithCursor.textContent);
+    spanWithCursor.textContent = removeCharAtIndex(cursorLetterPos, spanWithCursor.textContent);
+
+
+    let spanWithLetter = document.createElement("span");
+    spanWithLetter.textContent = letterWithCursor;
+    spanWithLetter.classList.add("text-cursor");
+    spanWithLetter.classList.add("untyped");
+    spanWithLetter.classList.add("default-font");
+
+    let textBeforeCursor = splitText[0];
+    let textAfterCursor = splitText[1];
+    console.log(textBeforeCursor);
+    console.log(textAfterCursor);
+    let node1 = document.createTextNode(textBeforeCursor);
+    let node2 = document.createTextNode(textAfterCursor);
+
+    spanWithCursor.innerHTML = "";
+    spanWithCursor.appendChild(node1);
+    spanWithCursor.appendChild(spanWithLetter);
+    spanWithCursor.appendChild(node2);
+}
+
+updateCursor();
 
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 } 
 
 function moveCursorForward() {
-    cursorPos++;
+    let wordLength = words[cursorWordPos].textContent.length;
+    if (cursorLetterPos + 1 >= wordLength && cursorWordPos + 1 < words.length) {
+        cursorLetterPos = 0;
+        cursorWordPos++;
+    }
+    else if (cursorLetterPos + 1 < wordLength) {cursorLetterPos++;}
+    updateCursor();
 }
 
 function moveCursorBackwards() {
-    if (cursorPos == 0) return;
-    cursorPos--;
-    
+    if (cursorLetterPos - 1 <= 0 && cursorWordPos != 0) {
+        cursorWordPos--;
+        let wordLength = words[cursorWordPos].textContent.length;
+        cursorLetterPos = wordLength - 1;
+    } else if (cursorLetterPos > 0) {
+        cursorLetterPos--;
+    }
+    updateCursor();
 }
 
 window.onkeydown = (event) => {
     console.log(event);
-    Array.prototype.forEach.call(words, word => {
-        console.log(word);
-    });
     if (event.key != null) {
         const typeEvent = event.key.length == 1;
-        if (typeEvent) {moveCursorForward();}
+        if (typeEvent && event.key != " ") {moveCursorForward();}
+        else if (event.key == "ArrowLeft") {moveCursorBackwards();}
+        else if (event.key == "ArrowRight") {moveCursorForward();}
     }
 
 } 
